2ebc0fa4b191cd37297c1c44bfc273b45f9266e6
1123581321345589144233377610987 <123220105+1123581321345589144233377610987@users.noreply.github.com>
2025-03-30 15:37:53 -0600

    Update mkdirSplitpath.c

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -5,19 +5,105 @@ extern struct NODE* cwd;
 
 //make directory
 void mkdir(char pathName[]){
-
-    // TO BE IMPLEMENTED
-    //
-    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
-
-    printf("TO BE IMPLEMENTED\n");
-
+    if(!(strcmp(pathName, "/"))){
+        printf("MKDIR ERROR: no path provided");
+        return;
+    }
+    
+    //allocate space for all the strings
+    char* baseName=(char*)malloc((strlen(pathName)+1)*sizeof(char));
+    char* dirName=(char*)malloc((strlen(pathName)+1)*sizeof(char));
+    struct NODE* currNode=(struct NODE*)malloc(sizeof(struct NODE));
+    currNode=splitPath(pathName,baseName,dirName);
+    struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+    if(currNode!=NULL){
+        newNode=currNode->childPtr;
+    }
+    else{
+        newNode=NULL;
+    }
+    while(newNode!=NULL){
+        if(!(strcmp(newNode->name,baseName))){
+            printf("MKDIR ERROR: directory %s already exists\n",baseName);
+            free(baseName);
+            free(dirName);
+            return;
+        }
+        newNode=newNode->siblingPtr;
+    }
+    
+    newNode->fileType='D';
+    strncpy(newNode->name,baseName,strlen(baseName));
+    newNode->name[strlen(baseName)]='\0';
+    newNode->childPtr=NULL;
+    newNode->siblingPtr=NULL;
+    printf("MKDIR SUCCESS: node %s successfully created\n",baseName);
+    free(baseName);
+    free(dirName);
     return;
+    
 }
 
 //handles tokenizing and absolute/relative pathing options
 struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
 
+    for(int i=strlen(pathName)-1;i>=0;i--){
+//setting dirName and baseName
+        if(i==0 || (i > 0 && pathName[i - 1] == '/')){
+            for(unsigned int j=i;j<strlen(pathName);j++){
+                baseName[j-i]=pathName[j];
+            }
+            baseName[strlen(pathName)-i]='\0';
+            if(i>1){
+                for(unsigned int k=0;k<i-1;k++){
+                    dirName[k]=pathName[k];
+                }
+                dirName[i-1]='\0';
+            }
+            else{
+                dirName[0]='\0';
+            }
+
+//initializing current node and child node
+            struct NODE* childNode=(struct NODE*)malloc(sizeof(struct NODE));
+            childNode=NULL;
+            struct NODE* currNode=(struct NODE*)malloc(sizeof(struct NODE));
+            currNode=NULL;
+            if(pathName[0]=='/'){
+                currNode=root;
+            }
+            else{
+                currNode=cwd;
+            }
+
+//initializing current directory name
+            char dirCopy[strlen(dirName) + 1];
+            strcpy(dirCopy, dirName);
+            char* currDirName = strtok(dirCopy, "/");
+
+            while(currDirName != NULL){
+                childNode = currNode->childPtr;
+                while(childNode!=NULL){
+                    if (strcmp(childNode->name,currDirName)==0) {
+                        break;
+                    }
+                    childNode = childNode->siblingPtr;
+                }
+
+                if (childNode==NULL){
+                        printf("ERROR: directory %s does not exist \n", currDirName);
+                        return NULL;
+                }
+                currNode = childNode; // we traverse the tree to the next depth with this assignment
+                currDirName = strtok(NULL,"/"); 
+            }
+            printf("DirName: %s/n",dirName);
+            printf("BaseName: %s/n",baseName);
+            return currNode;
+        }
+    }
+    return NULL;
+}
     // TO BE IMPLEMENTED
     // NOTE THAT WITHOUT COMPLETING THIS FUNCTION CORRECTLY
     // rm, rmdir, ls, cd, touch COMMANDS WILL NOT EXECUTE CORRECTLY
@@ -25,5 +111,4 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
 
     // YOUR CODE HERE
     //
-    return NULL;
-}
+
